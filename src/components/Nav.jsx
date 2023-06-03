1f180174f0a4b8f0d32a4e8b37ec7bfbe36939bf
sumit-chouhan-194 <sumitkhan.dhana@gmail.com>
2023-06-03 12:52:20 +0530

    start section

---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo saepe itaque, pariatur reprehenderit aliquid cupiditate iste iure autem eius aut. Corrupti quisquam animi modi iste culpa unde vitae accusantium nobis?</div>
+  )
+}
+export default Nav;
