5c314b8e44e79851a32eae2ca6b2faf7c153195c
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-06-03 12:18:01 +0530

    page start

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+      <>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta consectetur ducimus et laudantium modi eum aliquid ex iusto delectus labore assumenda pariatur, libero dolorum, eveniet impedit exercitationem corrupti esse? Commodi!</div>
+      </>
+  )
+}
+export default Nav;
